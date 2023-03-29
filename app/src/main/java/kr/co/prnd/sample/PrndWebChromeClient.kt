888c0dad473fa22a087fa64d44a99a6e381e9dc4
olaf-prnd <olaf@prnd.co.kr>
2023-03-29 14:54:24 +0900

    feat: 파일 선택 화면을 보여준다

--- app/src/main/java/kr/co/prnd/sample/PrndWebChromeClient.kt
+++ app/src/main/java/kr/co/prnd/sample/PrndWebChromeClient.kt
@@ -5,17 +5,18 @@ import android.net.Uri
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.widget.Toast
 
 class PrndWebChromeClient(
     private val context: Context,
 ) : WebChromeClient() {
+    private val fileChooser = FileChooser(context)
+
     override fun onShowFileChooser(
         webView: WebView?,
         filePathCallback: ValueCallback<Array<Uri>>?,
         fileChooserParams: FileChooserParams?
     ): Boolean {
-        Toast.makeText(context, "파일 선택 클릭", Toast.LENGTH_SHORT).show()
+        fileChooser.show()
         return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
     }
 }
